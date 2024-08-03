d9d6ac7f9f7308dbb310850e55f9c3474fba54cd
MIMANSHU MAHESHWARI <maheshwarimimanshu@gmail.com>
2024-08-03 08:52:36 +0530

    Update health check and its tests

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,6 @@ use enl::run;
 async fn main() -> std::io::Result<()> {
     let addr = "127.0.0.1:8000";
     let listener = TcpListener::bind(addr).expect("Failed to bind address to tcp listener");
+    println!("{:#?}", listener);
     run(listener)?.await
 }
